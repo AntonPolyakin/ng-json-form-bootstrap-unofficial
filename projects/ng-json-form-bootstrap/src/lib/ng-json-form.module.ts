135ef484a878c0477ce15a957309c0d607819954
Anton <anton16p@ya.ru>
2021-11-02 20:00:34 +0300

    0.1.0
    - angular 11

---
+++ projects/ng-json-form-bootstrap/src/lib/ng-json-form.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { NgJsonFormComponent } from './ng-json-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//import { BrowserModule } from '@angular/platform-browser';
+import { QuestionTextboxComponent } from './components/question-textbox/question-textbox.component';
+import { QuestionDropdownComponent } from './components/question-dropdown/question-dropdown.component';
+import { QuestionTextareaComponent } from './components/question-textarea/question-textarea.component';
+import { QuestionRangeComponent } from './components/question-range/question-range.component';
+import { QuestionCheckboxComponent } from './components/quesiton-checkbox/question-checkbox.component';
+import { QuestionRadioComponent } from './components/question-radio/question-radio.component';
+import { ValidationMessagesComponent } from './components/validation-messages/validation-messages.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    NgJsonFormComponent,
+    QuestionTextboxComponent,
+    QuestionDropdownComponent,
+    QuestionTextareaComponent,
+    QuestionRangeComponent,
+    QuestionCheckboxComponent,
+    QuestionRadioComponent,
+    ValidationMessagesComponent,
+  ],
+  imports: [
+    //BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [NgJsonFormComponent]
+})
+export class NgJsonFormModule { }
